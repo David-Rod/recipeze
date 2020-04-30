a1426b2606cae1bf24c52483160bffbcdb30a069
David Rodriguez <davidrod805@gmail.com>
2020-04-30 14:33:33 -0700

    Add snackbar (#1)
    * SnackBar message appears in loginPage
    
    * SnackBar message appears in createAccountPage

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -66,105 +66,107 @@ class _LoginPageState extends State<LoginPage> {
                 color: Colors.white, fontWeight: FontWeight.bold)),
       ),
     );
-    final loginButton = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          if (loginLogic.validateLoginCredentials(
-                  emailController.text, passwordController.text) ==
-              true) {
-            Navigator.pop(
-              context,
-            );
-          } else {
-            print("Invalid email or password. Please try again.");
-          }
-
-          // TODO: add the name of the class (main page) that this will
-          // navigate to
-          /*
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => <NameOfClassHere>()),
-          );
-          */
-        },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Center(
-        child: Container(
-          color: Colors.lightGreen,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Row(
-                  mainAxisSize: MainAxisSize.min,
+        resizeToAvoidBottomInset: false,
+        body: Builder(builder: (context) {
+          return Center(
+            child: Container(
+              color: Colors.lightGreen,
+              child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    SizedBox(width: 20.0, height: 100.0),
-                    Text(
-                      "RECIPEZE: ",
-                      style: TextStyle(
-                        fontSize: 30.0,
-                        fontWeight: FontWeight.bold,
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        SizedBox(width: 20.0, height: 100.0),
+                        Text(
+                          "RECIPEZE: ",
+                          style: TextStyle(
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(width: 20.0, height: 100.0),
+                        RotateAnimatedTextKit(
+                            onTap: () {
+                              print("Tap Event");
+                            },
+                            text: ["SIMPLE", "FAST", "FUN"],
+                            textStyle: TextStyle(
+                              fontSize: 30.0,
+                              fontFamily: "SourceSansPro-Light",
+                              fontWeight: FontWeight.bold,
+                            ),
+                            textAlign: TextAlign.start,
+                            alignment: AlignmentDirectional
+                                .topStart // or Alignment.topLeft
+                            ),
+                      ],
+                    ),
+                    Expanded(
+                      child: SizedBox(
+                        height: 100.0,
+                        width: 100.0,
+                        child: Image.asset(
+                          'images/chef_hat_cookbook.png',
+                          fit: BoxFit.contain,
+                        ),
                       ),
                     ),
-                    SizedBox(width: 20.0, height: 100.0),
-                    RotateAnimatedTextKit(
-                        onTap: () {
-                          print("Tap Event");
-                        },
-                        text: ["SIMPLE", "FAST", "FUN"],
-                        textStyle: TextStyle(
-                          fontSize: 30.0,
-                          fontFamily: "SourceSansPro-Light",
-                          fontWeight: FontWeight.bold,
-                        ),
-                        textAlign: TextAlign.start,
-                        alignment: AlignmentDirectional
-                            .topStart // or Alignment.topLeft
-                        ),
+                    SizedBox(height: 20.0),
+                    createAccountButton,
+                    SizedBox(height: 20.0),
+                    emailField,
+                    SizedBox(height: 20.0),
+                    passwordField,
+                    SizedBox(height: 20.0),
+                    Material(
+                        color: Color(0xff01A0C7),
+                        elevation: 5.0,
+                        borderRadius: BorderRadius.circular(30.0),
+                        child: MaterialButton(
+                          minWidth: MediaQuery.of(context).size.width,
+                          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+                          onPressed: () {
+                            if (loginLogic.validateLoginCredentials(
+                                    emailController.text,
+                                    passwordController.text) ==
+                                true) {
+                              /*
+                                Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => <NameOfClassHere>()),
+                                );
+                              */
+                            } else {
+                              SnackBar snackBar = SnackBar(
+                                content: Text(
+                                  'Invalid email or password.\nMinimum eight '
+                                  'characters, at least one uppercase letter, one lowercase letter and one number.',
+                                  textAlign: TextAlign.center,
+                                ),
+                              );
+                              Scaffold.of(context).showSnackBar(snackBar);
+                            }
+                          },
+                          child: Text("Login",
+                              textAlign: TextAlign.center,
+                              style: style.copyWith(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold)),
+                        )),
+                    SizedBox(
+                      height: 15.0,
+                    ),
                   ],
                 ),
-                Expanded(
-                  child: SizedBox(
-                    height: 100.0,
-                    width: 100.0,
-                    child: Image.asset(
-                      'images/chef_hat_cookbook.png',
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20.0),
-                createAccountButton,
-                SizedBox(height: 20.0),
-                emailField,
-                SizedBox(height: 20.0),
-                passwordField,
-                SizedBox(height: 20.0),
-                loginButton,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
-    );
+          );
+        }));
   }
 }
