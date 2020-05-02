fc1ccf61881a7b243fe6096f08e586273f5afea3
Dumilk <dariusdum@gmail.com>
2020-05-01 20:59:25 -0700

    bridged main screen with recipe info page

--- lib/recipe_info_page.dart
+++ lib/recipe_info_page.dart
@@ -62,8 +62,6 @@ class MealDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
         appBar: AppBar(
           title: Text(recipe.name),
@@ -80,7 +78,7 @@ class MealDetailScreen extends StatelessWidget {
                 height: 300,
                 width: double.infinity,
                 child: Image.network(
-                  'https://gbc-cdn-public-media.azureedge.net/img83669.768x512.jpg',
+                  recipe.imageURI,
                 )),
             buildSectionTitle(context, 'Preparation'),
             buildContainerPrep(
@@ -103,46 +101,6 @@ class MealDetailScreen extends StatelessWidget {
                   );
                 },
               ),
-      appBar: AppBar(
-        title: Text('Chiken Boi Recipe'),
-        leading: IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: () => { }),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.mode_edit), onPressed: () => { }),
-          IconButton(icon: Icon(Icons.info), onPressed: () {
-            Navigator.of(context).push(_createRoute());
-          },),
-        ],
-      ),
-
-      body: SingleChildScrollView(
-       child: Column(
-        children: <Widget>[
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              'https://gbc-cdn-public-media.azureedge.net/img83669.768x512.jpg',
-            )
-          ),
-
-
-          buildSectionTitle(context, 'Preparation'),
-          buildContainerPrep(
-            ListView.builder(
-              itemBuilder: (ctx, index) => Card(
-                color: Colors.white,
-                child: Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 5,
-                    ),
-                  child: Text("Maybe you add something here",
-                      style: TextStyle(color: Colors.black),
-                      textDirection: TextDirection.ltr)),
-              ),
-
-              //itemCount: selectedMeal.length
-              itemCount: 10,
             ),
             buildSectionTitle(context, 'Recipe Steps'),
             buildContainerSteps(
@@ -167,36 +125,5 @@ class MealDetailScreen extends StatelessWidget {
             ),
           ],
         )));
-  }
-}
-
-
-
-Route _createRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin = Offset(0.0, 1.0);
-      var end = Offset.zero;
-      var curve = Curves.ease;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
-}
-
-class Page2 extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Page 2'),
-      ),
-    );
   }
 }
